c845d802b9a0b48b93b64163000b8fe5fb07ffeb
jay0jim <284096905@qq.com>
2021-01-15 23:52:36 +0800

    ListView

--- FlutterDemo/lib/main.dart
+++ FlutterDemo/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
+import 'res/ListData.dart';
+
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -11,51 +13,100 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar( // 导航栏
+        appBar: AppBar(
+          // 导航栏
           title: Text('Flutter Demo'),
         ),
         body: HomeContent(), // 内容
-        backgroundColor: Colors.grey,
-        ),
-    ); 
+        backgroundColor: Colors.white,
+      ),
+    );
   }
 }
 
-// 内容组件
+// // 内容组件
+// class HomeContent extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       // 使用Container组件，类似于div
+//       child: Container(
+//         /// 加载本地图片
+//         child: Image.asset('images/test01.jpg'),
+
+//         // child: Image.network(
+//         //     'https://c-ssl.duitang.com/uploads/blog/202011/06/20201106134524_8560e.jpg'),
+//         // height: 300.0,
+//         // width: 300.0,
+//         // decoration: BoxDecoration(
+//         //   color: Colors.blue,
+//         //   // border: Border.all(
+//         //   //   color: Colors.black,
+//         //   //   width: 2.0,
+//         //   // ),
+//         //   borderRadius: BorderRadius.circular(150),
+//         //   image: DecorationImage(
+//         //     image: NetworkImage(
+//         //       'https://c-ssl.duitang.com/uploads/blog/202011/06/20201106134524_8560e.jpg',
+//         //       scale: 1.0,
+//         //     ),
+//         //     fit: BoxFit.cover,
+//         //   ),
+//         // ),
+//         // padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+//         // alignment: Alignment.center,
+
+//         /////
+//         /// ClipOval 裁剪圆形图片
+//         /// ClipRect 裁剪矩形图片
+//         /// ClipRRect 裁剪圆角矩形图片
+//         // child: ClipRRect(
+//         //   child: Image.network(
+//         //     'https://c-ssl.duitang.com/uploads/blog/202011/06/20201106134524_8560e.jpg',
+//         //     width: 300,
+//         //     height: 300,
+//         //   ),
+//         //   borderRadius: BorderRadius.circular(10),
+//         // ),
+//       ),
+//     );
+//   }
+// }
+
+// 列表ListView
+// class HomeContent extends StatelessWidget {
+//   List<Widget> _buildList() {
+//     var contentList = listData.map((e) {
+//       return ListTile(
+//         leading: Image.network(e['imageUrl']),
+//         title: Text(e['title']),
+//         subtitle: Text(e['author']),
+//       );
+//     });
+//     return contentList.toList();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView(
+//       children: this._buildList(),
+//     );
+//   }
+// }
+
+// ListView.builder 新建列表
 class HomeContent extends StatelessWidget {
+  Widget _getListContent(BuildContext context, int index) {
+    return ListTile(
+      leading: Image.network(listData[index]['imageUrl']),
+      title: Text(listData[index]['title']),
+      subtitle: Text(listData[index]['author']),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      // 使用Container组件，类似于div
-      child: Container(
-        child: Transform.rotate(
-          angle: math.pi/2,
-          alignment: Alignment.center,
-          child: Text(
-            'Hello, flutter.',
-            textDirection: TextDirection.ltr,
-            // textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.yellow,
-          ),
-        ),
-        ),
-        height: 300.0,
-        width: 300.0,
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          border: Border.all(
-            color: Colors.black,
-            width: 2.0,
-          ),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-        alignment: Alignment.center,
-        
-
-      ),
-    );
+    return ListView.builder(
+        itemCount: listData.length, itemBuilder: this._getListContent);
   }
 }
---
+++ FlutterDemo/lib/res/ListData.dart
@@ -0,0 +1,38 @@
+List listData = [
+  {
+    'title': 'Candy Shop',
+    'author': 'Tony',
+    'imageUrl': 'https://www.itying.com/images/flutter/1.png',
+  },
+  {
+    'title': 'There are moments in life when',
+    'author': 'you miss someone',
+    'imageUrl': 'https://www.itying.com/images/flutter/2.png',
+  },
+  {
+    'title': 'hug them for real',
+    'author': 'because ',
+    'imageUrl': 'https://www.itying.com/images/flutter/3.png',
+  },
+  {
+    'title': 'May you have enough',
+    'author': 'happiness',
+    'imageUrl': 'https://www.itying.com/images/flutter/4.png',
+  },
+  {
+    'title': 'to make you happy',
+    'author': 'probably',
+    'imageUrl': 'https://www.itying.com/images/flutter/5.png',
+  },
+  {
+    'title': 'necessarily have the',
+    'author': 'Happiness',
+    'imageUrl': 'https://www.itying.com/images/flutter/6.png',
+  },
+  {
+    'title': 'those who cry',
+    'author': 'searched',
+    'imageUrl': 'https://www.itying.com/images/flutter/7.png',
+  },
+
+];
